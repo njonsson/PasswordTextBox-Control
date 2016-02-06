1fe987deac3376148536a321526b8c6c74bb696d
Nils Jonsson <nils@alumni.rice.edu>
2016-02-06 17:53:53 -0600

    Change font from 8-pt Tahoma to 10-pt Calibri in the GUI test

--- PasswordTextBoxControl.Test.Gui/MainForm.Designer.cs
+++ PasswordTextBoxControl.Test.Gui/MainForm.Designer.cs
@@ -60,9 +60,10 @@
             // 
             this.PasswordTextBoxLabel.AutoEllipsis = true;
             this.PasswordTextBoxLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PasswordTextBoxLabel.Location = new System.Drawing.Point(16, 16);
+            this.PasswordTextBoxLabel.Location = new System.Drawing.Point(18, 18);
+            this.PasswordTextBoxLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PasswordTextBoxLabel.Name = "PasswordTextBoxLabel";
-            this.PasswordTextBoxLabel.Size = new System.Drawing.Size(190, 16);
+            this.PasswordTextBoxLabel.Size = new System.Drawing.Size(223, 18);
             this.PasswordTextBoxLabel.TabIndex = 0;
             this.PasswordTextBoxLabel.Text = "PasswordTextBox:";
             // 
@@ -70,9 +71,10 @@
             // 
             this.PasswordTextBox.AllowDrop = true;
             this.PasswordTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PasswordTextBox.Location = new System.Drawing.Point(16, 32);
+            this.PasswordTextBox.Location = new System.Drawing.Point(18, 36);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(190, 21);
+            this.PasswordTextBox.Size = new System.Drawing.Size(223, 23);
             this.PasswordTextBox.TabIndex = 1;
             this.PasswordTextBox.TextChanged += new System.EventHandler(this.PasswordTextBox_TextChanged);
             this.PasswordTextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxControl_DragDrop);
@@ -83,9 +85,10 @@
             // 
             this.UseSystemPasswordCharPropertyCheckbox.AutoEllipsis = true;
             this.UseSystemPasswordCharPropertyCheckbox.Dock = System.Windows.Forms.DockStyle.Top;
-            this.UseSystemPasswordCharPropertyCheckbox.Location = new System.Drawing.Point(16, 8);
+            this.UseSystemPasswordCharPropertyCheckbox.Location = new System.Drawing.Point(18, 9);
+            this.UseSystemPasswordCharPropertyCheckbox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.UseSystemPasswordCharPropertyCheckbox.Name = "UseSystemPasswordCharPropertyCheckbox";
-            this.UseSystemPasswordCharPropertyCheckbox.Size = new System.Drawing.Size(152, 17);
+            this.UseSystemPasswordCharPropertyCheckbox.Size = new System.Drawing.Size(179, 20);
             this.UseSystemPasswordCharPropertyCheckbox.TabIndex = 0;
             this.UseSystemPasswordCharPropertyCheckbox.Text = "UseSystemPasswordChar";
             this.UseSystemPasswordCharPropertyCheckbox.UseVisualStyleBackColor = true;
@@ -95,10 +98,11 @@
             // 
             this.PasswordCharPropertyTextBox.AllowDrop = true;
             this.PasswordCharPropertyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PasswordCharPropertyTextBox.Location = new System.Drawing.Point(16, 24);
+            this.PasswordCharPropertyTextBox.Location = new System.Drawing.Point(18, 27);
+            this.PasswordCharPropertyTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PasswordCharPropertyTextBox.MaxLength = 1;
             this.PasswordCharPropertyTextBox.Name = "PasswordCharPropertyTextBox";
-            this.PasswordCharPropertyTextBox.Size = new System.Drawing.Size(152, 21);
+            this.PasswordCharPropertyTextBox.Size = new System.Drawing.Size(179, 23);
             this.PasswordCharPropertyTextBox.TabIndex = 1;
             this.PasswordCharPropertyTextBox.TextChanged += new System.EventHandler(this.PasswordCharPropertyTextBox_TextChanged);
             this.PasswordCharPropertyTextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxControl_DragDrop);
@@ -109,9 +113,10 @@
             // 
             this.TextPropertyLabel.AutoEllipsis = true;
             this.TextPropertyLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.TextPropertyLabel.Location = new System.Drawing.Point(16, 4);
+            this.TextPropertyLabel.Location = new System.Drawing.Point(18, 5);
+            this.TextPropertyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.TextPropertyLabel.Name = "TextPropertyLabel";
-            this.TextPropertyLabel.Size = new System.Drawing.Size(152, 16);
+            this.TextPropertyLabel.Size = new System.Drawing.Size(179, 18);
             this.TextPropertyLabel.TabIndex = 0;
             this.TextPropertyLabel.Text = "Text:";
             // 
@@ -119,9 +124,10 @@
             // 
             this.TextPropertyTextBox.AllowDrop = true;
             this.TextPropertyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.TextPropertyTextBox.Location = new System.Drawing.Point(16, 20);
+            this.TextPropertyTextBox.Location = new System.Drawing.Point(18, 23);
+            this.TextPropertyTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.TextPropertyTextBox.Name = "TextPropertyTextBox";
-            this.TextPropertyTextBox.Size = new System.Drawing.Size(152, 21);
+            this.TextPropertyTextBox.Size = new System.Drawing.Size(179, 23);
             this.TextPropertyTextBox.TabIndex = 1;
             this.TextPropertyTextBox.TextChanged += new System.EventHandler(this.TextPropertyTextBox_TextChanged);
             this.TextPropertyTextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxControl_DragDrop);
@@ -132,9 +138,10 @@
             // 
             this.PasswordCharPropertyLabel.AutoEllipsis = true;
             this.PasswordCharPropertyLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PasswordCharPropertyLabel.Location = new System.Drawing.Point(16, 8);
+            this.PasswordCharPropertyLabel.Location = new System.Drawing.Point(18, 9);
+            this.PasswordCharPropertyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PasswordCharPropertyLabel.Name = "PasswordCharPropertyLabel";
-            this.PasswordCharPropertyLabel.Size = new System.Drawing.Size(152, 16);
+            this.PasswordCharPropertyLabel.Size = new System.Drawing.Size(179, 18);
             this.PasswordCharPropertyLabel.TabIndex = 0;
             this.PasswordCharPropertyLabel.Text = "PasswordChar:";
             // 
@@ -142,9 +149,10 @@
             // 
             this.PasswordCharDelayPropertyTextBox.AllowDrop = true;
             this.PasswordCharDelayPropertyTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PasswordCharDelayPropertyTextBox.Location = new System.Drawing.Point(16, 24);
+            this.PasswordCharDelayPropertyTextBox.Location = new System.Drawing.Point(18, 27);
+            this.PasswordCharDelayPropertyTextBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PasswordCharDelayPropertyTextBox.Name = "PasswordCharDelayPropertyTextBox";
-            this.PasswordCharDelayPropertyTextBox.Size = new System.Drawing.Size(152, 21);
+            this.PasswordCharDelayPropertyTextBox.Size = new System.Drawing.Size(179, 23);
             this.PasswordCharDelayPropertyTextBox.TabIndex = 1;
             this.PasswordCharDelayPropertyTextBox.TextChanged += new System.EventHandler(this.PasswordCharDelayPropertyTextBox_TextChanged);
             this.PasswordCharDelayPropertyTextBox.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextBoxControl_DragDrop);
@@ -156,9 +164,10 @@
             // 
             this.PasswordCharDelayPropertyLabel.AutoEllipsis = true;
             this.PasswordCharDelayPropertyLabel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PasswordCharDelayPropertyLabel.Location = new System.Drawing.Point(16, 8);
+            this.PasswordCharDelayPropertyLabel.Location = new System.Drawing.Point(18, 9);
+            this.PasswordCharDelayPropertyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.PasswordCharDelayPropertyLabel.Name = "PasswordCharDelayPropertyLabel";
-            this.PasswordCharDelayPropertyLabel.Size = new System.Drawing.Size(152, 16);
+            this.PasswordCharDelayPropertyLabel.Size = new System.Drawing.Size(179, 18);
             this.PasswordCharDelayPropertyLabel.TabIndex = 0;
             this.PasswordCharDelayPropertyLabel.Text = "PasswordCharDelay:";
             // 
@@ -172,9 +181,10 @@
             this.PasswordTextBoxPanel.Controls.Add(this.PasswordTextBoxLabel);
             this.PasswordTextBoxPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.PasswordTextBoxPanel.Location = new System.Drawing.Point(0, 0);
+            this.PasswordTextBoxPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PasswordTextBoxPanel.Name = "PasswordTextBoxPanel";
-            this.PasswordTextBoxPanel.Padding = new System.Windows.Forms.Padding(16, 16, 16, 8);
-            this.PasswordTextBoxPanel.Size = new System.Drawing.Size(222, 53);
+            this.PasswordTextBoxPanel.Padding = new System.Windows.Forms.Padding(18, 18, 18, 9);
+            this.PasswordTextBoxPanel.Size = new System.Drawing.Size(259, 61);
             this.PasswordTextBoxPanel.TabIndex = 0;
             // 
             // PropertiesGroupBox
@@ -184,9 +194,11 @@
             this.PropertiesGroupBox.Controls.Add(this.UseSystemPasswordCharPropertyPanel);
             this.PropertiesGroupBox.Controls.Add(this.TextPropertyPanel);
             this.PropertiesGroupBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PropertiesGroupBox.Location = new System.Drawing.Point(16, 12);
+            this.PropertiesGroupBox.Location = new System.Drawing.Point(18, 14);
+            this.PropertiesGroupBox.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PropertiesGroupBox.Name = "PropertiesGroupBox";
-            this.PropertiesGroupBox.Size = new System.Drawing.Size(190, 210);
+            this.PropertiesGroupBox.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.PropertiesGroupBox.Size = new System.Drawing.Size(223, 243);
             this.PropertiesGroupBox.TabIndex = 0;
             this.PropertiesGroupBox.TabStop = false;
             this.PropertiesGroupBox.Text = "Properties";
@@ -196,10 +208,11 @@
             this.PasswordCharDelayPropertyPanel.Controls.Add(this.PasswordCharDelayPropertyTextBox);
             this.PasswordCharDelayPropertyPanel.Controls.Add(this.PasswordCharDelayPropertyLabel);
             this.PasswordCharDelayPropertyPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PasswordCharDelayPropertyPanel.Location = new System.Drawing.Point(3, 148);
+            this.PasswordCharDelayPropertyPanel.Location = new System.Drawing.Point(4, 170);
+            this.PasswordCharDelayPropertyPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PasswordCharDelayPropertyPanel.Name = "PasswordCharDelayPropertyPanel";
-            this.PasswordCharDelayPropertyPanel.Padding = new System.Windows.Forms.Padding(16, 8, 16, 16);
-            this.PasswordCharDelayPropertyPanel.Size = new System.Drawing.Size(184, 59);
+            this.PasswordCharDelayPropertyPanel.Padding = new System.Windows.Forms.Padding(18, 9, 18, 18);
+            this.PasswordCharDelayPropertyPanel.Size = new System.Drawing.Size(215, 68);
             this.PasswordCharDelayPropertyPanel.TabIndex = 3;
             // 
             // PasswordCharPropertyPanel
@@ -207,20 +220,22 @@
             this.PasswordCharPropertyPanel.Controls.Add(this.PasswordCharPropertyTextBox);
             this.PasswordCharPropertyPanel.Controls.Add(this.PasswordCharPropertyLabel);
             this.PasswordCharPropertyPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.PasswordCharPropertyPanel.Location = new System.Drawing.Point(3, 95);
+            this.PasswordCharPropertyPanel.Location = new System.Drawing.Point(4, 109);
+            this.PasswordCharPropertyPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PasswordCharPropertyPanel.Name = "PasswordCharPropertyPanel";
-            this.PasswordCharPropertyPanel.Padding = new System.Windows.Forms.Padding(16, 8, 16, 8);
-            this.PasswordCharPropertyPanel.Size = new System.Drawing.Size(184, 53);
+            this.PasswordCharPropertyPanel.Padding = new System.Windows.Forms.Padding(18, 9, 18, 9);
+            this.PasswordCharPropertyPanel.Size = new System.Drawing.Size(215, 61);
             this.PasswordCharPropertyPanel.TabIndex = 2;
             // 
             // UseSystemPasswordCharPropertyPanel
             // 
             this.UseSystemPasswordCharPropertyPanel.Controls.Add(this.UseSystemPasswordCharPropertyCheckbox);
             this.UseSystemPasswordCharPropertyPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.UseSystemPasswordCharPropertyPanel.Location = new System.Drawing.Point(3, 66);
+            this.UseSystemPasswordCharPropertyPanel.Location = new System.Drawing.Point(4, 76);
+            this.UseSystemPasswordCharPropertyPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.UseSystemPasswordCharPropertyPanel.Name = "UseSystemPasswordCharPropertyPanel";
-            this.UseSystemPasswordCharPropertyPanel.Padding = new System.Windows.Forms.Padding(16, 8, 16, 8);
-            this.UseSystemPasswordCharPropertyPanel.Size = new System.Drawing.Size(184, 29);
+            this.UseSystemPasswordCharPropertyPanel.Padding = new System.Windows.Forms.Padding(18, 9, 18, 9);
+            this.UseSystemPasswordCharPropertyPanel.Size = new System.Drawing.Size(215, 33);
             this.UseSystemPasswordCharPropertyPanel.TabIndex = 1;
             // 
             // TextPropertyPanel
@@ -228,31 +243,34 @@
             this.TextPropertyPanel.Controls.Add(this.TextPropertyTextBox);
             this.TextPropertyPanel.Controls.Add(this.TextPropertyLabel);
             this.TextPropertyPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.TextPropertyPanel.Location = new System.Drawing.Point(3, 17);
+            this.TextPropertyPanel.Location = new System.Drawing.Point(4, 19);
+            this.TextPropertyPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.TextPropertyPanel.Name = "TextPropertyPanel";
-            this.TextPropertyPanel.Padding = new System.Windows.Forms.Padding(16, 4, 16, 8);
-            this.TextPropertyPanel.Size = new System.Drawing.Size(184, 49);
+            this.TextPropertyPanel.Padding = new System.Windows.Forms.Padding(18, 5, 18, 9);
+            this.TextPropertyPanel.Size = new System.Drawing.Size(215, 57);
             this.TextPropertyPanel.TabIndex = 0;
             // 
             // PropertiesPanel
             // 
             this.PropertiesPanel.Controls.Add(this.PropertiesGroupBox);
             this.PropertiesPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.PropertiesPanel.Location = new System.Drawing.Point(0, 53);
+            this.PropertiesPanel.Location = new System.Drawing.Point(0, 61);
+            this.PropertiesPanel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.PropertiesPanel.Name = "PropertiesPanel";
-            this.PropertiesPanel.Padding = new System.Windows.Forms.Padding(16, 12, 16, 16);
-            this.PropertiesPanel.Size = new System.Drawing.Size(222, 238);
+            this.PropertiesPanel.Padding = new System.Windows.Forms.Padding(18, 14, 18, 18);
+            this.PropertiesPanel.Size = new System.Drawing.Size(259, 275);
             this.PropertiesPanel.TabIndex = 1;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(222, 291);
+            this.ClientSize = new System.Drawing.Size(259, 336);
             this.Controls.Add(this.PropertiesPanel);
             this.Controls.Add(this.PasswordTextBoxPanel);
-            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.MinimumSize = new System.Drawing.Size(16, 329);
+            this.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MinimumSize = new System.Drawing.Size(16, 374);
             this.Name = "MainForm";
             this.Text = "PasswordTextBox Control GUI Test";
             ((System.ComponentModel.ISupportInitialize)(this.ErrorProvider)).EndInit();
